80eae3ca0cc98b9ac40c63a1fa66bfc50e339b86
Joshua Kerber <jkerber14@comcast.net>
2016-08-16 14:53:06 -0400

    updated title

--- src/components/app.js
+++ src/components/app.js
@@ -18,7 +18,7 @@ class App extends Component {
     return (
       <div>
         <div id="top-link-div">
-          <Link to="/"><h1 id="top-link">HW4 Blog by Josh Kerber</h1></Link>
+          <Link to="/"><h1 id="top-link">HW5 Blog by Josh Kerber</h1></Link>
         </div>
         {this.props.children}
       </div>
